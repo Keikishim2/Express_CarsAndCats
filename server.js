165ca0f95caade40b280a987221ce7f73ef9db81
Keikishim2 <Keikishim2@gmail.com>
2021-05-21 19:00:54 +0800

    first

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ let app = express();
 // this is the line that tells our server to use the "/static" folder for static content
 app.use(express.static(__dirname + "/static"));
 
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'ejs');
+
+app.get('/cats', function (request, response){
+    response.render('cats');
+})
+app.get('/cars', function (request, response){
+    response.render('cars');
+})
+app.get('/form', function (request, response){
+    response.render('cats');
+})
+
 
 // tell the express app to listen on port 8000, always put this at the end of your server.js file
 app.listen(8000, function() {
