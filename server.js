531178b4f9f8fd4a575f754bdb1e809b833608aa
Keikishim2 <Keikishim2@gmail.com>
2021-05-21 18:43:44 +0800

    first

---
+++ server.js
@@ -0,0 +1,14 @@
+// Load the express module and store it in the variable express (Where do you think this comes from?)
+let express = require('express');
+
+// invoke express and store the result in the variable app
+let app = express();
+
+// this is the line that tells our server to use the "/static" folder for static content
+app.use(express.static(__dirname + "/static"));
+
+
+// tell the express app to listen on port 8000, always put this at the end of your server.js file
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+})
